7c0d1508ab777f2089d3750235561fb85e2df97b
nbappi <bappi.cse562@gmail.com>
2016-07-26 15:41:31 +0600

    initial commit " How module work for node."

---
+++ index.js
@@ -0,0 +1,5 @@
+var myModule = require("calculator");
+
+console.log("Sum : " + myModule.easyMath([2, 4, 5], "+").result);
+console.log(myModule.easyMath([10, 6, 5], "*"));
+console.log(myModule.pi);
